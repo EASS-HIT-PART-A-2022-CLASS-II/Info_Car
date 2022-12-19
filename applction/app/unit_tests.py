c81a1f9ec197d77649ff11ca0cd0ff5e81fec7a6
Ubuntu <ubuntu@ip-172-31-45-224.eu-central-1.compute.internal>
2022-12-19 10:29:50 +0000

    added unit test

---
+++ applction/app/unit_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import requests
+import main
+
+class SmokeTest(unittest.TestCase):
+    def test_status_code(self):
+        self.assertEqual(requests.get("http://127.0.0.1").status_code, 200)
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
